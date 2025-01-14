661df429b766e5a16f01cec53356932a30733d97
Christian Wolf <christian.wolf@codesorcerer.de>
2025-01-14 10:29:26 +0100

    add missing rbac

--- internal/controller/wxonecluster_controller.go
+++ internal/controller/wxonecluster_controller.go
@@ -42,6 +42,8 @@ type WXOneClusterReconciler struct {
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=wxoneclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=wxoneclusters/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=wxoneclusters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=secrets;configmaps,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
